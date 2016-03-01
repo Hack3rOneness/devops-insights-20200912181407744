eeb7263f37b31c1a7688cb2b176129cec1f80762
Gulshan Singh <gsingh2011@gmail.com>
2016-03-01 01:54:30 -0800

    Install HHVM in the provision script

--- tools/provision.sh
+++ tools/provision.sh
@@ -3,6 +3,12 @@
 # Facebook CTF: Provision script for vagrant dev environment
 #
 
+# Install HHVM
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
+sudo add-apt-repository "deb http://dl.hhvm.com/ubuntu $(lsb_release -sc) main"
+sudo apt-get update
+sudo apt-get install hhvm
+
 # Remove apache configuration
 rm /etc/apache2/sites-enabled/scotchbox.local.conf
 
