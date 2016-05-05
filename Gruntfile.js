8fea2f2dcbfd46555f34d14570225621ccb4671d
Gulshan Singh <gsingh2011@gmail.com>
2016-05-04 22:07:07 -0700

    Don't use es2015 transform by default

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
           [
             'babelify', {
               presets: [
-                'es2015',
+                // 'es2015',
                 'react'
               ]
             }
