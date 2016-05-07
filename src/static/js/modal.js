6b23e49f58d0fcf458fd29e8f9d2114b4bde00c4
Gulshan Singh <gsingh2011@gmail.com>
2016-05-07 14:16:07 -0700

    Fixed modal bug

--- src/static/js/modal.js
+++ src/static/js/modal.js
@@ -39,84 +39,104 @@ function openAndLoad($modal, loadPath, cb) {
   });
 }
 
-module.exports = {
-  init: function() {
-    $modal = $('#fb-modal');
-    $modalContainer = $('#fb-main-content');
-    $countryHover = $('#fb-country-popup');
+function init() {
+  $modal = $('#fb-modal');
+  $modalContainer = $('#fb-main-content');
+  $countryHover = $('#fb-country-popup');
 
-    $('body').on('click', '.js-close-modal', this.close);
-  },
+  $('body').on('click', '.js-close-modal', close);
+}
 
-  close: function(event) {
-    if (event) {
-      event.preventDefault();
-    }
-
-    $('div[id^="fb-modal"]').removeClass(ACTIVE_CLASS);
-  },
-
-  /**
-   * there are two types of modals - default and popup. The
-   *  default modal takes up a full page, while the poup modal
-   *  creates a popup box for content. Both of these wrapper
-   *  functions take the same parameters.
-   */
-
-  load: function(modalName, className, cb) {
-    var modalClasses = DEFAULT_CLASSES + ' modal--' + className;
-    _load(modalName, modalClasses, cb);
-  },
-
-  loadPopup: function(modalParams, className, cb) {
-    var modalClasses = POPUP_CLASSES + ' modal--' + className;
-    _load(modalParams, modalClasses, cb);
-  },
-
-  /**
-   * create a persistent modal. This is used to build a modal
-   *  that is very specific, and should be loaded as quickly
-   *  as possible after initially loaded, like the command line
-   *  modal.
-   */
-  loadPersistent: function(modalParams, id, cb) {
-    var loadPath = 'index.php?' + modalParams,
-        modalId = 'fb-modal-persistent--' + id,
-        $modal = $(modalId);
-
-    if ($modal.length === 0) {
-      $modal = $('<div id="' + modalId + '" class="' + POPUP_CLASSES + '" />').appendTo($modalContainer);
-    }
-    Utils.loadComponent($modal, loadPath, cb);
-  },
-
-  openPersistent: function(id) {
-    var modalId = '#fb-modal-persistent--' + id;
-    $(modalId).addClass(ACTIVE_CLASS);
-  },
-
-  countryHoverPopup: function(cb) {
-    var loadPath = 'index.php?p=country&modal=popup';
-    if ($countryHover.length === 0) {
-      $countryHover = $('<div id="fb-country-popup" class="fb-popup-content popup--hover fb-section-border" />').appendTo($modalContainer);
-    }
-    openAndLoad($countryHover, loadPath, cb);
-  },
-
-  countryInactiveHoverPopup: function(cb) {
-    var loadPath = 'index.php?p=country&modal=inactive';
-    if ($countryHover.length === 0) {
-      $countryHover = $('<div id="fb-country-popup" class="fb-popup-content popup--hover fb-section-border" />').appendTo($modalContainer);
-    }
-    openAndLoad($countryHover, loadPath, cb);
-  },
-
-  viewmodePopup: function(cb) {
-    var loadPath = 'index.php?p=country&modal=viewmode';
-    if ($countryHover.length === 0) {
-      $countryHover = $('<div id="fb-country-popup" class="fb-popup-content popup--view-only" />').appendTo($modalContainer);
-    }
-
-    openAndLoad($countryHover, loadPath, cb);
+function close(event) {
+  if (event) {
+    event.preventDefault();
   }
+
+  $('div[id^="fb-modal"]').removeClass(ACTIVE_CLASS);
+}
+
+/**
+ * there are two types of modals - default and popup. The
+ *  default modal takes up a full page, while the poup modal
+ *  creates a popup box for content. Both of these wrapper
+ *  functions take the same parameters.
+ */
+
+function load(modalName, className, cb) {
+  var modalClasses = DEFAULT_CLASSES + ' modal--' + className;
+  _load(modalName, modalClasses, cb);
+}
+
+function loadPopup(modalParams, className, cb) {
+  var modalClasses = POPUP_CLASSES + ' modal--' + className;
+  _load(modalParams, modalClasses, cb);
+}
+
+/**
+ * create a persistent modal. This is used to build a modal
+ *  that is very specific, and should be loaded as quickly
+ *  as possible after initially loaded, like the command line
+ *  modal.
+ */
+function loadPersistent(modalParams, id, cb) {
+  var loadPath = 'index.php?' + modalParams,
+      modalId = 'fb-modal-persistent--' + id,
+      $modal = $(modalId);
+
+  if ($modal.length === 0) {
+    $modal = $('<div id="' + modalId + '" class="' + POPUP_CLASSES + '" />').appendTo($modalContainer);
+  }
+  Utils.loadComponent($modal, loadPath, cb);
+}
+
+function openPersistent(id) {
+  var modalId = '#fb-modal-persistent--' + id;
+  $(modalId).addClass(ACTIVE_CLASS);
+}
+
+function countryHoverPopup(cb) {
+  var loadPath = 'index.php?p=country&modal=popup';
+  if ($countryHover.length === 0) {
+    $countryHover = $('<div id="fb-country-popup" class="fb-popup-content popup--hover fb-section-border" />').appendTo($modalContainer);
+  }
+  openAndLoad($countryHover, loadPath, cb);
+}
+
+function countryInactiveHoverPopup(cb) {
+  var loadPath = 'index.php?p=country&modal=inactive';
+  if ($countryHover.length === 0) {
+    $countryHover = $('<div id="fb-country-popup" class="fb-popup-content popup--hover fb-section-border" />').appendTo($modalContainer);
+  }
+  openAndLoad($countryHover, loadPath, cb);
+}
+
+function viewmodePopup(cb) {
+  var loadPath = 'index.php?p=country&modal=viewmode';
+  if ($countryHover.length === 0) {
+    $countryHover = $('<div id="fb-country-popup" class="fb-popup-content popup--view-only" />').appendTo($modalContainer);
+  }
+
+  openAndLoad($countryHover, loadPath, cb);
+}
+
+module.exports = {
+  init: init,
+  // loads the basic modal
+  load: load,
+  // loads a persistent modal
+  loadPersistent: loadPersistent,
+  // open a persistent modal
+  openPersistent: openPersistent,
+  // load a popup modal
+  loadPopup: loadPopup,
+  // load and show the popup modal for a country hover
+  countryHoverPopup: countryHoverPopup,
+  // load and show the popup modal for an inactive country hover
+  countryInactiveHoverPopup: countryInactiveHoverPopup,
+  // load and show the view only country info
+  viewmodePopup: viewmodePopup,
+  // close the popup modal for a country hover
+  closeHoverPopup: closeHoverPopup,
+  // close the regular modal
+  close: close
 };
