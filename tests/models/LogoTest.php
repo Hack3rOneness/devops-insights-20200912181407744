2dabcb570ad61c359011f7a187e3bcb3a888bedd
Marjori Pomarole <marjoripomarole@gmail.com>
2016-05-07 22:18:15 +0100

    Adding LogoTest and moving model tests to /tests/models

---
+++ tests/models/LogoTest.php
@@ -0,0 +1,38 @@
+<?hh
+
+class LogoTest extends FBCTFTest {
+
+  public function testAllLogos(): void {
+    $all = HH\Asio\join(Logo::genAllLogos());
+    $this->assertEquals(1, count($all));
+
+    $l = $all[0];
+    $this->assertEquals(1, $l->getId());
+    $this->assertEquals('name', $l->getName());
+    $this->assertEquals('logo_path', $l->getLogo());
+    $this->assertTrue($l->getUsed());
+    $this->assertTrue($l->getEnabled());
+    $this->assertFalse($l->getProtected());
+  }
+
+  public function testCheckExists(): void {
+    $this->assertTrue(
+      HH\Asio\join(Logo::genCheckExists('name')),
+    );
+    $this->assertFalse(
+      HH\Asio\join(Logo::genCheckExists('no')),
+    );
+  }
+
+  public function testSetEnabled(): void {
+    HH\Asio\join(Logo::genSetEnabled(1, false));
+    $this->assertFalse(
+      HH\Asio\join(Logo::genCheckExists('name')),
+    );
+  }
+
+  public function testRandom(): void {
+    $s = HH\Asio\join(Logo::genRandomLogo());
+    $this->assertNotNull($s);
+  }
+}
