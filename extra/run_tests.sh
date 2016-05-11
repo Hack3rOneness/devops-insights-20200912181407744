86eae291b249f7719e967be8dc666d6e7f853d5b
Marjori Pomarole <marjoripomarole@gmail.com>
2016-05-11 07:50:15 +0200

    Fixing running tests on provision

--- extra/run_tests.sh
+++ extra/run_tests.sh
@@ -6,34 +6,27 @@
 #
 
 DB="fbctftests"
-USER="ctf"
-PWD="ctf"
 CODE_PATH=${1:-/vagrant}
+DB_USER=${2:-root}
+DB_PWD=${3:-root}
 
 echo "[+] Starting tests setup in $CODE_PATH"
 
-if ! [[ $(mysql_config_editor print --login-path=local) ]]; then
-  mysql_config_editor set --login-path=local --host=localhost --user="root" --password
-fi
-
-mysql --login-path=local -e "CREATE DATABASE $DB;"
-mysql --login-path=local -e "CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PWD';"
-mysql --login-path=local -e "GRANT ALL ON $DB.* TO '$USER'@'localhost';"
-mysql --login-path=local -e "FLUSH PRIVILEGES;"
-mysql --login-path=local "$DB" -e "source $CODE_PATH/database/test_schema.sql;"
-mysql --login-path=local "$DB" -e "source $CODE_PATH/database/logos.sql;"
-mysql --login-path=local "$DB" -e "source $CODE_PATH/database/countries.sql;"
+mysql -u "$DB_USER" --password="$DB_PWD" -e "CREATE DATABASE $DB;"
+mysql -u "$DB_USER" --password="$DB_PWD" -e "FLUSH PRIVILEGES;"
+mysql -u "$DB_USER" --password="$DB_PWD" "$DB" -e "source $CODE_PATH/database/test_schema.sql;"
+mysql -u "$DB_USER" --password="$DB_PWD" "$DB" -e "source $CODE_PATH/database/logos.sql;"
+mysql -u "$DB_USER" --password="$DB_PWD" "$DB" -e "source $CODE_PATH/database/countries.sql;"
 
 echo "[+] DB Connection file"
-cat "$CODE_PATH/extra/settings.ini.example" | sed "s/DATABASE/$DB/g" | sed "s/MYUSER/$USER/g" | sed "s/MYPWD/$PWD/g" > "$CODE_PATH/settings.ini"
+cat "$CODE_PATH/extra/settings.ini.example" | sed "s/DATABASE/$DB/g" | sed "s/MYUSER/$DB_USER/g" | sed "s/MYPWD/$DB_PWD/g" > "$CODE_PATH/settings.ini"
 
 echo "[+] Starting tests"
 hhvm --config tests/server.ini vendor/phpunit/phpunit/phpunit --configuration tests/phpunit.xml tests
 
 echo "[+] Deleting test database"
-mysql --login-path=local -e "DROP DATABASE IF EXISTS $DB;"
-mysql --login-path=local -e "DROP USER '$USER'@'localhost'";
-mysql --login-path=local -e "FLUSH PRIVILEGES;"
+mysql -u="$DB_USER" --password="$DB_PWD" -e "DROP DATABASE IF EXISTS $DB;"
+mysql -u="$DB_USER" --password="$DB_PWD" -e "FLUSH PRIVILEGES;"
 
 exit 0
 
