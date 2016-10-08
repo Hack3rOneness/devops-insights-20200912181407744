a8ab8988e827b9b8202c70090964635852790a1d
Fred Emmott <fred@fredemmott.co.uk>
2016-10-08 02:38:32 -0700

    Disallow running scripts from web (#264)

--- src/scripts/bases.php
+++ src/scripts/bases.php
@@ -1,5 +1,10 @@
 <?hh
 
+if (php_sapi_name() !== 'cli') {
+  http_response_code(405); // method not allowed
+  exit(0);
+}
+
 require_once ('/var/www/fbctf/vendor/autoload.php');
 
 $conf_game = \HH\Asio\join(Configuration::gen('game'));
