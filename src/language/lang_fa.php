973fba1a162e45d80f40136cc0bee4b9d927bfbe
Mehrdad <m.abdi@aut.ac.ir>
2016-11-23 18:49:38 +0330

    farsi language is added.

---
+++ src/language/lang_fa.php
@@ -0,0 +1,669 @@
+﻿<?hh // strict
+
+/* HH_IGNORE_ERROR[1002] */
+$translations = array(
+  'date and time format' =>
+    'H:i:s D m/d/Y', //used by date() function
+  //Translations for IndexController
+  'Facebook CTF' =>
+    'Facebook CTF',
+  'Conquer the world' =>
+    'تسخیر جهان',
+  'Play' =>
+    'بازی کنید',
+  'Welcome to the Facebook Capture the Flag Competition. By clicking "Play," you will be entered into the official CTF challenge. Good luck in your conquest.' =>
+    'به رقابت فتح پرچم فیسبوک خوش آمدید. با کلیک روی دکمه "بازی کنید" به محیط مسابقه وارد خواهید شد. موفق باشید.',
+  'Get ready for the CTF to start and access the gameboard now!' =>
+    'برای مسابقه آماده باشید و به صفحه بازی بروید!',
+  'Gameboard' =>
+    'صفحه بازی',
+  'Register Team' =>
+    'ثبت نام تیم',
+  'Get ready for the CTF to start and register your team now!' =>
+    'برای مسابقه آماده باشید و تیم مورد نظر خود را ثبت کنید',
+  'Login' =>
+    'ورود',
+  'Soon' =>
+    'به زودی',
+  'Upcoming Game' =>
+    'بازی پیش رو',
+  '_days' =>
+    '_روز',
+  '_hours' =>
+    '_ساعت',
+  '_minutes' =>
+    '_دقیقه',
+  '_seconds' =>
+    '_ثانیه',
+  'Official CTF Rules' =>
+    'قوانین رسمی مسابقه',
+  'Following actions are prohibited, unless explicitly told otherwise by event Admins.' =>
+    'اقدامات زیر ممنوع است مگر اینکه به صراحت توسط تیم برگزار کننده بیان شده باشد.',
+  'Rule' =>
+    'قانون',
+  'Cooperation' =>
+    'همکاری',
+  'No cooperation between teams with independent accounts. Sharing of keys or providing revealing hints to other teams is cheating, don’t do it.' =>
+    'تیم ها نباید با هم همکاری داشته باشند. اشتراک گذاشتن جواب چالش ها یا راهنمایی کردن سایر تیم ها تقلب محسوب می شود.',
+  'Attacking Scoreboard' =>
+    'حمله به زیر ساخت مسابقه',
+  'No attacking the competition infrastructure. If bugs or vulns are found, please alert the competition organizers immediately.' =>
+    'به هیچ وجه به زیر ساخت مسابقه حمله نکنید. اگر باگ یا آسیب پذیری کشف کردید، لطفا آن را به تیم برگزار کننده گزارش دهید.',
+  'Sabotage' =>
+    'خرابکاری',
+  'Absolutely no sabotaging of other competing teams, or in any way hindering their independent progress.' =>
+    'هیچ گونه خرابکاری در روند مسابقه دادن سایر تیم ها ایجاد نکنید.',
+  'Bruteforcing' =>
+    'بروت فورس',
+  'No brute forcing of challenge flag/ keys against the scoring site.' =>
+    'هیچ گونه بروت فورس روی سایت مسابقه برای بدست آوردن کلید مسابقه نزنید.',
+  'Denial Of Service' =>
+    'ممانعت از سرویس',
+  'DoSing the CTF platform or any of the challenges is forbidden.' =>
+    'هرگونه اقدام منجر به ممانعت از سرویس روی زیرساخت مسابقه یا هریک از سوالات ممنوع است.',
+  'Legal' =>
+    'حقوقی',
+  'Disclaimer' =>
+    'سلب مسئولیت',
+  'By participating in the contest, you agree to release Facebook and its employees, and the hosting organization from any and all liability, claims or actions of any kind whatsoever for injuries, damages or losses to persons and property which may be sustained in connection with the contest. You acknowledge and agree that Facebook et al is not responsible for technical, hardware or software failures, or other errors or problems which may occur in connection with the contest.' =>
+    'By participating in the contest, you agree to release Facebook and its employees, and the hosting organization from any and all liability, claims or actions of any kind whatsoever for injuries, damages or losses to persons and property which may be sustained in connection with the contest. You acknowledge and agree that Facebook et al is not responsible for technical, hardware or software failures, or other errors or problems which may occur in connection with the contest.',
+  'If you have any questions about what is or is not allowed, please ask an organizer.' =>
+    'اگر درباره ممنوع بودن یا نبودن اقدامی ابهام دارید، حتما از برگزار کنندگان مسابقه سوال کنید.',
+  'Have fun!' =>
+    'لذت ببرید!',
+  'Name' =>
+    'نام',
+  'Email' =>
+    'ایمیل',
+  'Token' =>
+    'توکن',
+  'Team Registration' =>
+    'ثبت نام تیم',
+  'Team Name' =>
+    'نام تیم',
+  'Password' =>
+    'پسورد',
+  'Choose an Emblem' =>
+    'یک علامت انتخاب کنید',
+  'Sign Up' =>
+    'ثبت نام',
+  'Register to play Capture The Flag here. Once you have registered, you will be logged in.' =>
+    'در اینجا برای شرکت در رقابت فتح پرچم ثبت نام کنید. بعد از ثبت نام موفق به سایت مسابقه وارد می شوید.',
+  'Not Available' =>
+    'در دسترس نیست',
+  'Team Registration will be open soon, stay tuned!' =>
+    'ثبت نام تیم ها به زودی فعال خواهد شد. در ارتباط باشید!',
+  'Try Again' =>
+    'دوباره تلاش کنید',
+  'Select' =>
+    'انتخاب',
+  'Team Login' =>
+    'ورود تیم',
+  'Please login here. If you have not registered, you may do so by clicking "Sign Up" below. ' =>
+    'لطفا وارد شوید. اگر هنوز ثبت نام نکرده اید، می توانید با کلیک روی دکمه "ثبت نام" این کار را انجام دهید.',
+  'Team Login will be open soon, stay tuned!' =>
+    'ورود تیم به زودی فعال خواهد شد. در ارتباط باشید!',
+  'ERROR' =>
+    'خطا',
+  'Start Over' =>
+    'دوباره آغاز کنید',
+  'Window is too small' =>
+    'پنجره تان خیلی کوچک است',
+  'For the best CTF experience, please make window size bigger.' =>
+    'برای داشتن تجربه بهتر از مسابقه، لطفا پنجره را بزرگتر بکنید.',
+  'Thank you.' =>
+    'با سپاس.',
+  'Logout' =>
+    'خروج',
+  'Registration' =>
+    'ثبت نام',
+  'Play CTF' =>
+    'آغاز رقابت',
+  'Rules' =>
+    'قوانین',
+  //Translations for GameboardController
+  'Admin' =>
+    'Admin',
+  'ADMIN' =>
+    'ادمین',
+  'Navigation' =>
+    'راهبری',
+  'View Mode' =>
+    'حالت مشاهده',
+  'View mode' =>
+    'حالت مشاهده',
+  'Tutorial' =>
+    'آموزش',
+  'Scoreboard' =>
+    'جدول امتیازها',
+  'You' =>
+    'شما',
+  'Others' =>
+    'سایرین',
+  'All' =>
+    'همه',
+  'Leaderboard' =>
+    'تیم های برتر',
+  'Announcements' =>
+    'اعلانات',
+  'Teams' =>
+    'تیم ها',
+  'Filter' =>
+    'فیلتر کردن',
+  'Activity' =>
+    'فعالیت',
+  'Game Clock' =>
+    'ساعت مسابقه',
+  //Translations for AdminController
+  'Auto' =>
+    'خودکار',
+  'All Categories' =>
+    'همه دسته بندی ها',
+  'Open' =>
+    'باز',
+  'Tokenized' =>
+    'توکن دار',
+  'Hour' =>
+    'ساعت',
+  'Hours' =>
+    'ساعت',
+  'Used by' =>
+    'استفاه شده توسط',
+  'Used By' =>
+    'استفاده شده توسط',
+  'Available' =>
+    'در دسترس',
+  'Registration Tokens' =>
+    'توکن ثبت نام',
+  'Create More' =>
+    'ساخت بیشتر',
+  'Export Available' =>
+    'اکسپورت موجود است',
+  'Not started yet' =>
+    'هنوز شروع نشده است',
+  'Configuration' =>
+    'تنظیمات',
+  'Tokens' =>
+    'توکن ها',
+  'Game Configuration' =>
+    'تنظیمات بازی',
+  'OK' =>
+    'اوکی',
+  'status_' =>
+    'وضعیت_',
+  'On' =>
+    'روشن',
+  'Off' =>
+    'خاموش',
+  'Player Names' =>
+    'اسامی بازیکنان',
+  'Players Per Team' =>
+    'بازیکنان به ازای هر تیم',
+  'Registration Type' =>
+    'نوع ثبت نام',
+  'Strong Passwords' =>
+    'رمز قوی',
+  'Team Selection' =>
+    'انتخاب تیم',
+  'Game' =>
+    'بازی',
+  'Scoring' =>
+    'امتیاز دهی',
+  'Progressive Cycle (s)' =>
+    'Progressive Cycle  (s)',
+  'Refresh Gameboard' =>
+    'Refresh Gameboard',
+  'Default Bonus' =>
+    'جایزه پیش فرض',
+  'Bases Cycle (s)' =>
+    'Bases Cycle (s)',
+  'Default Bonus Dec' =>
+    'کاهش پیش فرض جایزه',
+  'Timer' =>
+    'تایمر',
+  'Server Time' =>
+    'زمان سرور',
+  'Game Duration' =>
+    'مدت بازی',
+  'Begin Time' =>
+    'شروع بازی',
+  'Expected End Time' =>
+    'زمان مورد انتظار برای پایان',
+  'Language' =>
+    'زبان',
+  'DELETE' =>
+    'حذف',
+  'Delete' =>
+    'حذف',
+  'No Announcements' =>
+    'هیچ اعلانی وجود ندارد',
+  'Game Controls' =>
+    'کنترلهای بازی',
+  'Write New Announcement here' =>
+    'اعلان جدید را اینجا بنویسید',
+  'Create' =>
+    'بساز',
+  'General' =>
+    'عمومی',
+  'Back Up Database' =>
+    'بکاپ دیتابیس',
+  'Export Full Game' =>
+    'اکسپورت کامل بازی',
+  'Import Full Game' =>
+    'ایمپورت کامل بازی',
+  'Import Teams' =>
+    'ایمپورت تیم ها',
+  'Export Teams' =>
+    'اکسپورت تیم ها',
+  'Import Logos' =>
+    'ایمپورت لوگوها',
+  'Export Logos' =>
+    'اکسپورت لوگوها',
+  'Import Levels' =>
+    'ایمپورت چالش ها',
+  'Export Levels' =>
+    'اکسپورت چالش ها',
+  'Import Categories' =>
+    'ایمپورت دسته ها',
+  'Export Categories' =>
+    'اکسپورت دسته ها',
+  'Levels' =>
+    'چالش ها',
+  'New Quiz Level' =>
+    'چالش آموزه جدید',
+  'Title' =>
+    'عنوان',
+  'Question' =>
+    'سوال',
+  'Level title' =>
+    'عنوان سوال',
+  'Quiz question' =>
+    'سوال آموزه',
+  'Country' =>
+    'کشور',
+  'Answer' =>
+    'جواب',
+  'Points' =>
+    'امتیاز',
+  'Hint' =>
+    'راهنمایی',
+  'Hint Penalty' =>
+    'پنالتی راهنمایی',
+  'EDIT' =>
+    'راهنمایی',
+  'All Quiz Levels' =>
+    'همه ی آموزه ای',
+  'Filter By:' =>
+    'فیلتر با:',
+  'All Status' =>
+    'همه وضعیت ها',
+  'Enabled' =>
+    'فعال',
+  'Disabled' =>
+    'غیرفعال',
+  'Quiz Level' =>
+    'سوال آموزه',
+  'Show Answer' =>
+    'پاسخ کوتاه',
+  'Bonus' =>
+    'امتیاز',
+  '-Dec' =>
+    '-منفی',
+  'Save' =>
+    'ذخیره',
+  'Quiz Management' =>
+    'مدیریت آموزه ها',
+  'Add Quiz Level' =>
+    'اضافه کردن سوال آموزه',
+  'New Flag Level' =>
+    'سوال آموزه جدید',
+  'Description' =>
+    'توضیح',
+  'Level description' =>
+    'توضیح سوال',
+  'Category' =>
+    'دسته',
+  'Flag' =>
+    'پرچم',
+  'flag' =>
+    'پرچم',
+  'All Flag Levels' =>
+    'همه ی چالش های پرچمی',
+  'New Attachment:' =>
+    'پیوست جدید:',
+  'Attachment' =>
+    'پیوست',
+  'Link' =>
+    'لینک',
+  'New Link:' =>
+    'لینک جدید:',
+  'Flag Level' =>
+    'چالش پرچمی',
+  'Categories' =>
+    'دسته ها',
+  '+ Attachment' =>
+    '+ پیوست',
+  '+ Link' =>
+    '+ لینک',
+  'Flags Management' =>
+    'مدیریت چالشهای پرچمی',
+  'Add Flag Level' =>
+    'اضافه کردن چالش پرچمی',
+  'New Base Level' =>
+    'چالش جدید پایگاهی',
+  'Keep Points' =>
+    'امتیاز حفظ',
+  'Capture points' =>
+    'امتیاز فتح',
+  'All Base Levels' =>
+    'همه ی چالش های پایگاهی',
+  'Base Level' =>
+    'چالش پایگاه',
+  'Bases Management' =>
+    'مدیریت پایگاه ها',
+  'Add Base Level' =>
+    'اضافه کردن چالش پایگاه',
+  'New Category' =>
+    'دسته جدید',
+  'Category: ' =>
+    'دسته: ',
+  'Categories Management' =>
+    'مدیریت دسته ها',
+  'Add Category' =>
+    'اضافه کردن دسته ',
+  'All Countries' =>
+    'همه کشورها',
+  'In Use' =>
+    'درحال استفاده',
+  'In use' =>
+    'درحال استفاده',
+  'Not Used' =>
+    'استفاده نشده',
+  'Yes' =>
+    'آری',
+  'No' =>
+    'خیر',
+  'ISO Code' =>
+    'کد ایزو',
+  'Countries Management' =>
+    'مدیریت کشورها',
+  'No Team Names' =>
+    'اسم تیمی وجود ندارد',
+  'time' =>
+    'زمان',
+  'type' =>
+    'نوع',
+  'pts' =>
+    'امتیاز',
+  'Level' =>
+    'چالش',
+  'level' =>
+    'چالش',
+  'No Scores' =>
+    'هیچ امتیاز',
+  'Attempt' =>
+    'تلاش',
+  'No Failures' =>
+    'هیچ اشتباه',
+  'Team' =>
+    'تیم',
+  'team' =>
+    'تیم',
+  'Names' =>
+    'اسامی',
+  'Scores' =>
+    'امتیازها',
+  'Failures' =>
+    'اشتباه ها',
+  'New Team' =>
+    'تیم جدید',
+  'Team Logo' =>
+    'لوگو تیم جدید',
+  'Selected Logo:' =>
+    'لوگو انتخاب شده:',
+  'Select Logo' =>
+    'لوگو انتخاب کنید',
+  'All Teams' =>
+    'همه تیم ها',
+  'Protected' =>
+    'محافظت شده',
+  'Score' =>
+    'امتیاز',
+  'Change Password' =>
+    'تغییر پسورد',
+  'Admin Level' =>
+    'سطح ادمین',
+  'Visibility' =>
+    'قابل مشاهده',
+  'Team Management' =>
+    'مدیریت تیم',
+  'Add Team' =>
+    'اضافه کردن تیم',
+  'None' =>
+    'هیچ',
+  'Logo Name' =>
+    'اسم لوگو',
+  'Logo Management' =>
+    'مدیریت لوگو',
+  'Session' =>
+    'نشست',
+  'Cookie' =>
+    'کوکی',
+  'Creation Time' =>
+    'زمان ساخت',
+  'Last Access' =>
+    'آخرین دسترسی',
+  'Last Page Access' =>
+    'آخرین دسترسی به صفحه',
+  'Data' =>
+    'تاریخ',
+  'Sessions' =>
+    'نشست ها',
+  'entry' =>
+    'ورودی',
+  'No Entries' =>
+    'هیچ ورودی ای',
+  'Game Logs' =>
+    'لاگ بازی',
+  'Game Logs Timeline' =>
+    'تایم لاین لاگ بازی',
+  'End Game' =>
+    'پایان مسابقه',
+  'Begin Game' =>
+    'شروع مسابقه',
+  'Game Admin' =>
+    'ادمین بازی',
+  'Controls' =>
+    'کنترل ها',
+  'Quiz' =>
+    'آموزه',
+  'Flags' =>
+    'پرچمی',
+  'Bases' =>
+    'پایگاهی',
+  'Countries' =>
+    'کشورها',
+  'Logos' =>
+    'لوگوها',
+  //Translations for inc/* and inc/gameboard/*
+  'captured' =>
+    'فتح شده',
+  'Status' =>
+    'وضعیت',
+  'Completed' =>
+    'تمام شده',
+  'Remaining' =>
+    'باقی مانده',
+  'Start' =>
+    'شروع',
+  'End' =>
+    'پایان',
+  'Rank' =>
+    'رتبه',
+  'pts' =>
+    'امتیاز', //points
+  'Your Rank' =>
+    'رتبه شما',
+  'Your Score' =>
+    'امتیاز شما',
+  'Everyone' =>
+    'هرکس',
+  'Your Team' =>
+    'تیم شما',
+  'Captured' =>
+    'فتح شده',
+  'Initiating' =>
+    'درحال آماده سازی',
+  'run : > boot_sequence' =>
+    'game@ctfserver$ boot_sequence',
+  'Extracting' =>
+    'استخراجینگ',
+  //Translations for Utils.php's time_ago() function
+  'just now' =>
+    'همین الان',
+  'd' =>
+    'روز', //day
+  'hr' =>
+    'ساعت', //hour
+  'min' =>
+    'دقیقه', //minute
+  'sec' =>
+    'ثانیه', //second
+  'ds' =>
+    'روز', //days
+  'hrs' =>
+    'ساعت', //hours
+  'mins' =>
+    'دقیقه', //minutes
+  'secs' =>
+    'ثانیه', //seconds
+  'ago' =>
+    'پیش',
+  //Translations for ModalControllers
+  'begin_' =>
+    'شروع_',
+  'Are you sure you want to kick off the game? Logs will be cleared and progressive scoreboard will start' =>
+    'آیا مطئن هستید که می خواهید بازی را شروع کنید؟ همه لاگ ها حذف می شوند و اسکوربورد شروع به کار خواهد کرد',
+  'end_' =>
+    'پایان_',
+  'Are you sure you want to finish the current game?' =>
+    'مطمئن هستید که می خواهید بازی را تمام کنید؟',
+  'Are you sure you want to logout from the game?' =>
+    'مطمئن هستید که می خواهید لاگ اوت کنید؟',
+  'Saved' =>
+    'ذخیره شده',
+  'All changes have been successfully saved.' =>
+    'همه تغییرات با موفقیت ذخیره شدند.',
+  'Error' =>
+    'خطا',
+  'Sorry your form was not saved. Please correct the all errors and save again.' =>
+    'شرمنده! اطلاعات ذخیره نشدند. خطاها را بررسی کرده و دوباره ذخیره کنید.',
+  'cancel_' =>
+    'انصراف_',
+  'Are you sure you want to cancel? You have unsaved changes that will be reverted.' =>
+    'آیا مطمئن هستید که می خواهید کنسل کنید؟ تغییرات ذخیره نشده خواهند پرید.',
+  'choose_logo' =>
+    'انتخاب_لوگو',
+  'captured_' =>
+    'فتح_شده_',
+  'flag_owner_' =>
+    'صاحب_پرچم_',
+  'INACTIVE' =>
+    'غیرفعال',
+  'PTS' =>
+    'امتیاز',
+  'category' =>
+    'دسته',
+  'capture_' =>
+    'فتح_',
+  'Insert your answer' =>
+    'پاسخ خود را وارد کنید',
+  'Request Hint' =>
+    'درخواست راهنمایی',
+  'Submit' =>
+    'ارسال',
+  'hint_' =>
+    'راهنمایی_',
+  'first_capture' =>
+    'اولین_فتح',
+  'completed_by' =>
+    'حل شده_توسط',
+  'scoreboard_' =>
+    'جدول امتیازها_',
+  'filter_' =>
+    'فیلتر_',
+  'rank_' =>
+    'رتبه_',
+  'team_name_' =>
+    'اسم_تیم_',
+  'quiz_pts_' =>
+    'امتیاز_آموزه_',
+  'flag_pts_' =>
+    'امتیاز_پرچم_',
+  'base_pts_' =>
+    'امتیاز_پایگاه_',
+  'total_pts_' =>
+    'کل_امتیازها_',
+  'team_' =>
+    'تیم_',
+  'team_members' =>
+    'اعضای_تیم',
+  'base_pts' =>
+    'امتیاز_پایگاه',
+  'quiz_pts' =>
+    'امتیاز_آموزه',
+  'flag_pts' =>
+    'امتیاز_پرچم',
+  'total_pts' =>
+    'کل_امتیاز',
+  'Tool bars are located on all edges of the gameboard. Tap a category to expand and close each tool bar.' =>
+    'Tool bars are located on all edges of the gameboard. Tap a category to expand and close each tool bar.',
+  'Tool_Bars' =>
+    'Tool_Bars',
+  'Tap the "Game Clock" to keep track of time during gameplay. Don’t let time get the best of you.' =>
+    'Tap the "Game Clock" to keep track of time during gameplay. Don’t let time get the best of you.',
+  'Game_Clock' =>
+    'Game_Clock',
+  'Countries marked with an ' =>
+    'Countries marked with an ',
+  'are captured by you.' =>
+    'are captured by you.',
+  ' are owned by others.' =>
+    ' are owned by others.',
+  'Captures' =>
+    'Captures',
+  'Tap Plus[+] to Zoom In. Tap Minus[-] to Zoom Out.' =>
+    'استفاد کنید از [+] برای زوم کردن. و همچنین از[-] برای دور شدن.',
+  'Click and Drag to move left, right, up and down.' =>
+    'کلیک و دراگ برای حرکت به چپ راست بالا و پایین.',
+  'Zoom' =>
+    'زوووم',
+  'Tap Forward Slash [/] to activate computer commands. A list of commands can be found under "Rules".' =>
+    'با زدن دکمه [/] روی صفحه کلید خط فرمان دستورات را باز کنید. لیستی از دستورات را می توانید آنجا ببینید.',
+  'Command_Line' =>
+    'خط_فرمان',
+  'Click "Nav" to access main navigation links like Rules of Play, Registration, Blog, Jobs & more.' =>
+    'روی "ناو" کلیک کنید تا لینک های اصلی مانند قوانین بازی، ثبت نام و بیاید.',
+  'Track your competition by clicking "scoreboard" to access real-time game statistics and graphs.' =>
+    'وضعیت بازی را با مشاهده "اسکوربورد" و آمار آنلاین بازی رصد کنید.',
+  'Have fun, be the best and conquer the world.' =>
+    'لذت ببرید، بهترین باشید و جهان را تسخیر کنید.',
+  'Game_On' =>
+    'بازی_فعال',
+  'tutorial_' =>
+    'آموزش_',
+  'Next' =>
+    'بعدی',
+  'Skip to play' =>
+    'برو به بازی',
+  'Powered By Facebook' =>
+    'ساخته شده توسط فیسبوک',
+  'Active Directory / LDAP' =>
+    'Active Directory / LDAP',
+  'LDAP Server' =>
+    'LDAP Server',
+  'LDAP Port' =>
+    'LDAP Port',
+  'LDAP Domain' =>
+    'LDAP Domain',
+);
