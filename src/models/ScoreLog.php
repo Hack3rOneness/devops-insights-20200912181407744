e181da25b18c2534ef44f60510e702ef1b9695df
ubuntu <justin@wraysec.com>
2017-08-05 02:44:21 -0400

    Fixed conflict in ScoreLog

--- src/models/ScoreLog.php
+++ src/models/ScoreLog.php
@@ -293,33 +293,6 @@ class ScoreLog extends Model {
     MultiTeam::invalidateMCRecords('TEAMS_FIRST_CAP'); // Invalidate Memcached MultiTeam data.
   }
 
-  public static async function genScoreLogUpdate(
-    int $level_id,
-    int $team_id,
-    int $points,
-    string $type,
-    string $timestamp,
-  ): Awaitable<void> {
-    $db = await self::genDb();
-    await $db->queryf(
-      'UPDATE scores_log SET ts = %s, level_id = %d, team_id = %d, points = %d, type = %s WHERE level_id = %d AND team_id = %d',
-      $timestamp,
-      $level_id,
-      $team_id,
-      $points,
-      $type,
-      $level_id,
-      $team_id,
-    );
-    self::invalidateMCRecords(); // Invalidate Memcached ScoreLog data.
-    Control::invalidateMCRecords('ALL_ACTIVITY'); // Invalidate Memcached Control data.
-    MultiTeam::invalidateMCRecords('ALL_TEAMS'); // Invalidate Memcached MultiTeam data.
-    MultiTeam::invalidateMCRecords('POINTS_BY_TYPE'); // Invalidate Memcached MultiTeam data.
-    MultiTeam::invalidateMCRecords('LEADERBOARD'); // Invalidate Memcached MultiTeam data.
-    MultiTeam::invalidateMCRecords('TEAMS_BY_LEVEL'); // Invalidate Memcached MultiTeam data.
-    MultiTeam::invalidateMCRecords('TEAMS_FIRST_CAP'); // Invalidate Memcached MultiTeam data.
-  }
-
   public static async function genUpdateScoreLogBonus(
     int $level_id,
     int $team_id,
